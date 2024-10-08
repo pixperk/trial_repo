d138adfdb4f59d312b3949a009d66fbef579dd06
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-10-08 17:56:09 +0530

    New Feature Revised

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const age = 24;
 const callHim = "John Doe";
 const dob = "01/01/2000";
 
-
+function newFeature(){
+    console.log("New Feauture Alert!");
+    console.log("New Feauture Alert wohoo!");
+    
+}

dd8d39ff2cedfe45de3a7999ccfe790db147a720
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-10-08 18:08:49 +0530

    feat-b

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const dob = "01/01/2000";
 
 function newFeature(){
     console.log("New Feauture Alert!");
-    console.log("New Feauture Alert wohoo!");
+    console.log("New Feauture BRANCH B");
     
 }
